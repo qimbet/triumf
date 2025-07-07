e2256df7e294eeecb3a82ca4db95588b4bd7c671
Jacob Mattie <j_mattie@live.ca>
2025-07-06 21:07:37 -0700

    First release of install.sh

--- install.sh
+++ install.sh
@@ -33,12 +33,13 @@ else
 fi
 
 
+debDirName='debFiles'
+githubDirName='gitRepos'
 
-mainDirPath='main/directory/path'
-debFolderPath='path/to/folder/withDebFiles'
-githubFilesPath='path/to/github/installation/dir'
+debDirPath="$mainDirPath/$debDirName"
+gitDirPath="$mainDirPath/$githubDirName"
 
-
+mkdir -p /home/EPICS/
 mkdir -p /opt/epics/extensions/src
 
 apt-get update
@@ -53,23 +54,23 @@ apt --fix-broken install
 #single quotes are better for outer wrappings
 
 linesForBashrc=(
-  'export PATH="$PATH:$coreDir"'
+  'export PATH="$PATH:$mainDirPath"'
   'export PATH="$PATH:$debFolderPath"'
   'export PATH="$PATH:$githubFilesPath"'
-  "export PATH='/opt/epics/epics-base/bin/linux-x86_64:$PATH'"
-  
-  "export EPICS_BASE='/opt/epics/epics-base'"
-  "export EPICS_HOST_ARCH='linux-x86_64'"
-  "export EPICS_CA_AUTO_ADDR_LIST='YES'"
-  "export EPICS_EXTENSIONS='/opt/epics/extensions'"
-  "export PATH='$EPICS_EXTENSIONS/bin/$EPICS_HOST_ARCH:$PATH'"
-  "export EDMPVOBJECTS='$EPICS_EXTENSIONS/src/edm/setup'"
-  "export EDMOBJECTS='$EPICS_EXTENSIONS/src/edm/setup'"
-  "export EDMHELPFILES='$EPICS_EXTENSIONS/src/edm/helpFiles'"
-  "export EDMFILES='$EPICS_EXTENSIONS/src/edm/edmMain'"
-  "export EDMLIBS='$EPICS_EXTENSIONS/lib/$EPICS_HOST_ARCH'"
-)
 
+  'export PATH="/opt/epics/epics-base/bin/linux-x86_64:$PATH"'
+  'export PATH="$EPICS_BASE/bin/linux-x86_64:$PATH"'
+  'export EPICS_BASE="/opt/epics/epics-base"'
+  'export EPICS_HOST_ARCH="linux-x86_64"'
+  'export EPICS_CA_AUTO_ADDR_LIST="YES"'
+  'export EPICS_EXTENSIONS="/opt/epics/extensions"'
+  'export PATH="$EPICS_EXTENSIONS/bin/$EPICS_HOST_ARCH:$PATH"'
+  'export EDMPVOBJECTS="$EPICS_EXTENSIONS/src/edm/setup"'
+  'export EDMOBJECTS="$EPICS_EXTENSIONS/src/edm/setup"'
+  'export EDMHELPFILES="$EPICS_EXTENSIONS/src/edm/helpFiles"'
+  'export EDMFILES="$EPICS_EXTENSIONS/src/edm/edmMain"'
+  'export EDMLIBS="$EPICS_EXTENSIONS/lib/$EPICS_HOST_ARCH"'
+)
 
 for line in "${linesForBashrc[@]}"; do
 	echo "$line" >> ~/.bashrc
@@ -121,34 +122,31 @@ sudo apt --fix-broken install -y
 #                d delete
 #                i insert BEFORE selected line
 #                a append AFTER selected line
-#                s substitute part of the line (regex) -- 10s/foo/bar -- replaces first instance (on line 10) of foo with bar
+#                s substitute part of the line (regex) -- 10s/foo/bar -- replaces first instance of foo with bar
 #                        Can append the argument /g to substitute ALL occurrences
 #
 
-sudo sed -i -e '21cEPICS_BASE=/opt/epics/epics-base' -e '25s/^/#/' /opt/epics/epics-base/configure/RELEASE
-sudo sed -i -e '14cX11_LIB=/usr/lib/x86_64-linux-gnu' -e '18cMOTIF_LIB=/usr/lib/x86_64-linux-gnu' /opt/epics/extensions/configure/os/CONFIG_SITE.linux-x86_64.linux-x86_64
+sed -i -e '21cEPICS_BASE=/opt/epics/epics-base' -e '25s/^/#/' 					/opt/epics/epics-base/configure/RELEASE
+sed -i -e '14cX11_LIB=/usr/lib/x86_64-linux-gnu' -e '18cMOTIF_LIB=/usr/lib/x86_64-linux-gnu' 	/opt/epics/extensions/configure/os/CONFIG_SITE.linux-x86_64.linux-x86_64
 
-sudo sed -i -e '15s/$/ -DGIFLIB_MAJOR=5 -DGIFLIB_MINOR=1/' /opt/epics/extensions/src/edm/giflib/Makefile
-sudo sed -i -e 's| ungif||g' /opt/epics/extensions/src/edm/giflib/Makefile*
+sed -i -e '15s/$/ -DGIFLIB_MAJOR=5 -DGIFLIB_MINOR=1/' 						/opt/epics/extensions/src/edm/giflib/Makefile
+sed -i -e 's| ungif||g' 									/opt/epics/extensions/src/edm/giflib/Makefile*
 
-#for 
-sudo sed -i -e '53cfor libdir in baselib lib epicsPv locPv calcPv util choiceButton pnglib diamondlib giflibvideowidget' /opt/epics/extensions/src/setup/edm/setup.sh
-sudo sed -i -e '79d' /opt/epics/extensions/src/setup/edm/setup.sh
-sudo sed -i -e '81i\ \ \ \ $EDM -add $EDMBASE/pnglib/O.$ODIR/lib57d79238-2924-420b-ba67-dfbecdf03fcd.so' /opt/epics/extensions/src/setup/edm/setup.sh
-sudo sed -i -e '82i\ \ \ \ $EDM -add $EDMBASE/diamondlib/O.$ODIR/libEdmDiamond.so' /opt/epics/extensions/src/setup/edm/setup.sh
-sudo sed -i -e '83i\ \ \ \ $EDM -add $EDMBASE/giflib/O.$ODIR/libcf322683-513e-4570-a44b-7cdd7cae0de5.so' /opt/epics/extensions/src/setup/edm/setup.sh
-sudo sed -i -e '84i\ \ \ \ $EDM -add $EDMBASE/videowidget/O.$ODIR/libTwoDProfileMonitor.so' /opt/epics/extensions/src/setup/edm/setup.sh
+sed -i -e '53cfor libdir in baselib lib epicsPv locPv calcPv util choiceButton pnglib diamondlib giflibvideowidget' /opt/epics/extensions/src/setup/edm/setup.sh
+sed -i -e '79d' 											/opt/epics/extensions/src/setup/edm/setup.sh
+sed -i -e '81i\ \ \ \ $EDM -add $EDMBASE/pnglib/O.$ODIR/lib57d79238-2924-420b-ba67-dfbecdf03fcd.so' 	/opt/epics/extensions/src/setup/edm/setup.sh
+sed -i -e '82i\ \ \ \ $EDM -add $EDMBASE/diamondlib/O.$ODIR/libEdmDiamond.so' 				/opt/epics/extensions/src/setup/edm/setup.sh
+sed -i -e '83i\ \ \ \ $EDM -add $EDMBASE/giflib/O.$ODIR/libcf322683-513e-4570-a44b-7cdd7cae0de5.so' 	/opt/epics/extensions/src/setup/edm/setup.sh
+sed -i -e '84i\ \ \ \ $EDM -add $EDMBASE/videowidget/O.$ODIR/libTwoDProfileMonitor.so' 			/opt/epics/extensions/src/setup/edm/setup.sh
 
 
 #---------------------------------------------------------
 #
-#		Appends lines to bashrc
+#		Builds EPICS
 #
 #---------------------------------------------------------
-cd /opt/epics/epics-base
-sudo make
 
-cd /opt/epics/extensions/src/edm
-sudo make clean
-sudo make
+/opt/epics/epics-base/make
+/opt/epics/extensions/src/edm/make clean
+/opt/epics/extensions/src/edm/make
 
